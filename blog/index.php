cb223eeea537594b54fb26a6ecdc0732c8d7819a
aziz zaaine <zaaine@live.fr>
2024-10-15 17:38:09 +0200

    first commit

---
+++ blog/index.php
@@ -0,0 +1,35 @@
+<?php
+// index.php
+
+require_once('src/controllers/add_comment.php');
+require_once('src/controllers/homepage.php');
+require_once('src/controllers/post.php');
+
+try {
+	if (isset($_GET['action']) && $_GET['action'] !== '') {
+    	if ($_GET['action'] === 'post') {
+        	if (isset($_GET['id']) && $_GET['id'] > 0) {
+            	$identifier = $_GET['id'];
+
+            	post($identifier);
+        	} else {
+            	throw new Exception('Aucun identifiant de billet envoyé');
+        	}
+    	} elseif ($_GET['action'] === 'addComment') {
+        	if (isset($_GET['id']) && $_GET['id'] > 0) {
+            	$identifier = $_GET['id'];
+
+            	addComment($identifier, $_POST);
+        	} else {
+            	throw new Exception('Aucun identifiant de billet envoyé');
+        	}
+    	} else {
+        	throw new Exception("La page que vous recherchez n'existe pas.");
+    	}
+	} else {
+    	homepage();
+	}
+} catch (Exception $e) { // S'il y a eu une erreur, alors...
+	echo 'Erreur : '.$e->getMessage();
+    require('templates/error.php');
+}
---
+++ blog/src/controllers/post.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once('src/model.php');
+require_once('src/model/comment.php');
+
+
+function post(string $identifier)
+{
+    $post = getPost($identifier);
+    $comments = getComments($identifier);
+
+    require('templates/post.php');
+}
