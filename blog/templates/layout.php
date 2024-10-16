ca06f5f8a0583c3b79b664160b53105a09aa31b2
aziz zaaine <zaaine@live.fr>
2024-10-16 10:23:08 +0200

    ajout d'une barNav

--- blog/templates/layout.php
+++ blog/templates/layout.php
@@ -4,12 +4,27 @@
         <meta charset="utf-8" />
         <title><?= $title ?></title>
         <link rel="stylesheet" href="src/styles/style.css">
-
     </head>
 
     <body>
-        <?= $content ?>
-    </body>
+  
+        <!-- En-tête avec la navigation -->
+        <header>
+            <nav class="navbar">
+                <ul class="navbarlist">
+                <li class="navbarListLink"><a href="#">Le Club</a></li>
+                <li class="navbarListLink"><a href="#">Sports</a></li>
+                <li class="navbarListLink"><a href="#">Stages</a></li>
+                <li class="navbarListLink"><a href="#">PUC Social Club</a></li>
+                <li class="navbarListLink"><a href="#">Art & Sport</a></li>
+                </ul>
+            </nav>
+        </header>
 
-    <footer> Copyright Zaaine Aziz</footer>
+        <!-- Contenu de la page -->
+        <?= $content ?>
+
+        <!-- Footer -->
+        <footer> Copyright Zaaine Aziz</footer>
+    </body>
 </html>
